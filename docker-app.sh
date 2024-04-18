556a95d3c09708b8b77caacdb5e29f587bad4fc8
Marco Lizza <marco.lizza@gmail.com>
2024-04-18 18:25:53 +0200

    Updating `docker` script.

--- docker-app.sh
+++ docker-app.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-wget -O- https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/docker-archive-keyring.gpg > /dev/null
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+NAME=docker
+KEY_URL=https://download.docker.com/linux/ubuntu/gpg
+APT_URL=https://download.docker.com/linux/ubuntu
+VERSION=$(lsb_release -cs)
+CHANNEL=stable
+
+wget -O- ${KEY_URL} | gpg --dearmor | sudo tee /usr/share/keyrings/${NAME}-archive-keyring.gpg > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/${NAME}-archive-keyring.gpg] ${APT_URL} ${VERSION} ${CHANNEL}" | sudo tee /etc/apt/sources.list.d/${NAME}.list > /dev/null
